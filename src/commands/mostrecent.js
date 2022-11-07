e05ebb1fa820dad01034dab701eeabb263257d5a
Austin Gillmann <austingillmann@gmail.com>
2022-11-07 16:03:02 -0600

    commands now working

--- src/commands/mostrecent.js
+++ src/commands/mostrecent.js
@@ -1,57 +1,73 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+
+const getChase = async () => {
+    // axios GET request to https://us-central1-chaseapp-8459b.cloudfunctions.net/ListChases with X-ApiKey
+    const response = await axios.get('https://us-central1-chaseapp-8459b.cloudfunctions.net/ListChases', {
+        headers: {
+            'X-ApiKey': '8b373c2d-41bc-4a18-80f3-b3671f04930f',
+            'Content-Type': 'application/json',
+        }
+    })
+    return response.data;
+}
 
 module.exports = {
+
 data: new SlashCommandBuilder()
     .setName("mostrecent")
     .setDescription("Displays info from most recent chase"),
 
 async execute(interaction, client) {
-    // GET request to https://us-central1-chaseapp-8459b.cloudfunctions.net/ListChases
-    request.get({
-        headers: {'X-ApiKey': '8b373c2d-41bc-4a18-80f3-b3671f04930f', 'Content-Type': 'application/json'},
-        url:    'https://us-central1-chaseapp-8459b.cloudfunctions.net/ListChases',
-    }, function(error, response, body){
-        var json = JSON.parse(body);
-        var mostRecent = json[0];
-        var mostRecentID = mostRecent.ID;
-        var mostRecentTitle = mostRecent.Name;
-        var mostRecentDescription = mostRecent.Desc;
-        var mostRecentStartTime = mostRecent.startTime;
-        var mostRecentEndTime = mostRecent.endTime;
-        var mostRecentImageURL = mostRecent.imageURL;
 
+    json = await getChase();
+    mostRecent = json[0];
+    console.log(mostRecent);
+    mostRecentID = mostRecent.ID;
+    console.log(mostRecentID);
+    mostRecentTitle = mostRecent.Name;
+    console.log(mostRecentTitle);
+    mostRecentDescription = mostRecent.Desc;
+    console.log(mostRecentDescription);
+    mostRecentStartTime = mostRecent.CreatedAt;
+    console.log(mostRecentStartTime);
+    mostRecentEndTime = mostRecent.EndedAt;
+    console.log(mostRecentEndTime);
+    mostRecentImageURL = mostRecent.ImageURL;
+    console.log(mostRecentImageURL);
 
-        const chaseembed = new EmbedBuilder()
-            .setColor("#5865f4")
-            .setTitle(":green_circle:" + `${mostRecentTitle}`)
-            .setThumbnail(`${mostRecentImageURL}`)
-            .setImage(`${mostRecentImageURL}`)
-            .addFields(
-            {
-                ID: `${mostRecentID}`,
-                Description: `${mostRecentDescription}`,
-                "Start Time": `${mostRecentStartTime}`,
-            }
-            )
-            .setTimestamp();
-            //append each network to the embed as a field
-            for (var i = 0; i < mostRecent.networks.length; i++) {
-                chaseembed.addField(mostRecent.networks[i].name, mostRecent.networks[i].url);
-            }
-            // if chase is live, add :red_circle: to live field, else add :white_circle: and end time
-            if (mostRecent.live) {
-                chaseembed.addField("Live", ":red_circle:");
-            } else {
-                chaseembed.addField("Live", ":white_circle: " + `${mostRecentEndTime}`);
-            }
-        const button = new ActionRowBuilder().addComponents(
-            new ButtonBuilder()
-            .setLabel('chaseapp.tv')
-            .setStyle(5)
-            .setEmoji('💻')
-            .setURL('https://chaseapp.tv/'),
-        );
-    });
+    const chaseembed = new EmbedBuilder()
+        .setColor("#5865f4")
+        .setTitle(":green_circle:" + `${mostRecentTitle}`)
+        .setThumbnail(`${mostRecentImageURL}`)
+        .setImage(`${mostRecentImageURL}`)
+        .addFields(
+            { name: 'ID', value: `${mostRecentID}` },
+            { name: 'Description', value: `${mostRecentDescription}` },
+            { name: 'Start Time', value: `${mostRecentStartTime}` },
+        )
+        .setTimestamp();
+        //append each network to the embed as a field
+        for (var i = 0; i < mostRecent.Networks.length; i++) {
+            chaseembed.addFields(
+                { name: 'Networks', value: `${mostRecent.Networks[i].Name}` },
+                { name: 'URL', value: `${mostRecent.Networks[i].URL}` }
+                );
+        }
+        // if chase is live, add :red_circle: to live field, else add :white_circle: and end time
+        if (mostRecent.live) {
+            chaseembed.addFields({ name: 'live', value: ':red_circle:' });
+        } else {
+            chaseembed.addFields({ name: 'live', value: ':white_circle:' });
+        }
+    const button = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+        .setLabel('chaseapp.tv')
+        .setStyle(5)
+        .setEmoji('💻')
+        .setURL('https://chaseapp.tv/'),
+    );
+
 
         await interaction.reply({
             embeds: [chaseembed],
--- src/commands/setadmin.js
+++ src/commands/setadmin.js
@@ -1,29 +1,35 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const axios = require('axios');
 
 module.exports = {
-data: new SlashCommandBuilder()
-    .setName("setadmin")
-    .setDescription("Sets admin role within chaseapp firestore")
-    .addUserOption(option => option.setName('UID').setDescription('The UID to set as admin').setRequired(true))
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-
+    data: new SlashCommandBuilder()
+        .setName("setadmin")
+        .setDescription("Sets admin role within chaseapp firestore")
+        .addStringOption((option) => 
+            option.setName('uid')
+                .setDescription('The UID to set as admin')
+                .setRequired(true))
+                .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 async execute(interaction, client) {
-    // POST request to set admin role using UID parameter to https://us-central1-chaseapp-8459b.cloudfunctions.net/SetAdmin
-    request.post({
-        headers: {'X-ApiKey': '8b373c2d-41bc-4a18-80f3-b3671f04930f', 'Content-Type': 'application/json'},
-        url:    'https://us-central1-chaseapp-8459b.cloudfunctions.net/SetAdmin',
-        body:   `{ "uid": "${interaction.options.get('UID').value}" }`
-    })
-
-    const adminembed = new EmbedBuilder()
-    .setColor("#5865f4")
-    .setTitle(":green_circle:  Admin Role Set!")
-    .addFields(
-        {
-            uid: `${interaction.options.get('UID').value}`,
+    // axios POST request to set admin role using UID parameter and X-ApiKey of 8b373c2d-41bc-4a18-80f3-b3671f04930f to https://us-central1-chaseapp-8459b.cloudfunctions.net/SetAdmin
+    axios.post('https://us-central1-chaseapp-8459b.cloudfunctions.net/SetAdmin', {
+        uid: interaction.options.getString('uid'),
+    }, {
+        headers: {
+            'X-ApiKey': '8b373c2d-41bc-4a18-80f3-b3671f04930f',
+            'Content-Type': 'application/json',
         }
-    )
-    .setTimestamp();
+    })
+    .then(function (response) {global.response = response})
+    
+    const adminembed = new EmbedBuilder()
+        .setColor("#5865f4")
+        .setTitle(":green_circle:  Admin Role Set!")
+        .addFields(
+            {name: "UID", value: `${interaction.options.get('uid').value}`},
+            {name: "Response", value: `${global.response.data}`},
+        )
+        .setTimestamp();
 
     const button = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
