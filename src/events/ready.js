d9d360d956ae4cdc1b417d7043cfbad8bb55eb3b
Austin Gillmann <austingillmann@gmail.com>
2022-11-09 21:43:05 -0600

    support recursive folder struct

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,15 +4,30 @@ const { readdirSync } = require("fs");
 require("dotenv").config();
 const { ChalkAdvanced } = require("chalk-advanced");
 
+//function that recursively scans the src/commands subfolders and folder for files that end in js and returns an array of the file paths
+function getFiles(dir, files_ = []) {
+    const files = readdirSync(dir);
+    for (const file of files) {
+        const name = dir + "/" + file;
+        //check if file or folder
+        if (!file.includes(".")) {
+            getFiles(name, files_);
+        } else if (file.endsWith(".js")) {
+          //push file path to array with ./src/ removed
+            files_.push(name.replace("./src/", "../"));
+        }
+    }
+    return files_;
+}
+
 module.exports = async (client) => {
-  const commandFiles = readdirSync("./src/commands/").filter((file) =>
-    file.endsWith(".js")
-  );
+//scan for files that end in js in the src/commands subfolders and add them to the commandFiles array
+const commandFiles = getFiles("./src/commands");
 
   const commands = [];
 
   for (const file of commandFiles) {
-    const command = require(`../commands/${file}`);
+    const command = require(`${file}`);
     try { 
       commands.push(command.data.toJSON());
     } catch (error) {
@@ -32,7 +47,7 @@ module.exports = async (client) => {
         await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID), {
           body: commands,
         });
-        console.log(`${ChalkAdvanced.white("anima ٩(◕‿◕｡)۶")} ${ChalkAdvanced.gray(">")} ${ChalkAdvanced.green("Successfully registered commands locally")}`);
+        console.log(`${ChalkAdvanced.white("anima ")} ${ChalkAdvanced.gray(">")} ${ChalkAdvanced.green("Successfully registered commands locally")}`);
 
       } else {
         await rest.put(
